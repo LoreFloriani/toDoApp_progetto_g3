077e709fe5b693c485d590b00e0d661571538c5d
AlbertoCavallari <alberto.cavallari@buonarroti.tn.it>
2026-01-02 16:28:26 +0100

    Fix al file del login. Aggiungta la funzione get username by username

--- core/functions.php
+++ core/functions.php
@@ -1,17 +1,26 @@
 <?php
     function handle_login($pdo, $username, $password) {
-    $user = get_user_by_username($pdo, $username);
+        $user = get_user_by_username($pdo, $username);
 
-    if ($user && password_verify($password, $user['password'])) {
-        // LOGIN RIUSCITO
-        session_regenerate_id(true);
+        if ($user && password_verify($password, $user['password'])) {
+            // LOGIN RIUSCITO
+            session_regenerate_id(true);
 
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];  
-        
-        return true;
-    } else {
-        return "Username o password non validi.";
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];  
+            
+            return true;
+        } else {
+            return "Username o password non validi.";
+        }
     }
-}
+
+    function get_user_by_username($pdo, $username) {
+        $sql = "SELECT * FROM utenti WHERE username = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$username]);
+        
+        // fetch() restituisce la riga trovata, o 'false' se non trova nulla.
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 ?>
